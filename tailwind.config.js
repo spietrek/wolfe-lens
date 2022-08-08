10040e93a77c03c57f373e3cd46c627c608be32d
spietrek <spietrek@gmail.com>
2022-08-08 09:22:12 -0400

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [path.join(__dirname, './src/**/*.(js|jsx|ts|tsx)')],
+  // important: true,
   // important: '#root',
   darkMode: 'class',
   theme: {
@@ -14,16 +15,6 @@ module.exports = {
   },
   variants: {
     extend: {},
-    backgroundColor: [
-      'dark',
-      'dark-hover',
-      'dark-group-hover',
-      'dark-even',
-      'dark-odd',
-    ],
-    borderColor: ['dark', 'dark-disabled', 'dark-focus', 'dark-focus-within'],
-    textColor: ['dark', 'dark-hover', 'dark-active', 'dark-placeholder'],
-    opacity: ['responsive', 'hover', 'focus', 'disabled'],
   },
   plugins: [
     require('@tailwindcss/typography'),
