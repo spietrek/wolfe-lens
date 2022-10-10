4827b1f99c9e87c381d0c8a8e70e31ca163e4d5c
spietrek <spietrek@gmail.com>
2022-10-10 10:23:30 -0400

    fix home template

--- src/components/templates/InsHome.tsx
+++ src/components/templates/InsHome.tsx
@@ -1,41 +1,47 @@
 import { PropsWithChildren } from 'react'
-import { IProduct, IProductItem } from '../../types/product.type'
+import { ICategory } from '../../types/category.type'
+import { ICommunity } from '../../types/community.type'
+import { IHeroWithButton } from '../../types/hero.type'
+import { IProductItem } from '../../types/product.type'
 import InsIndicator from '../molecules/InsIndicator'
 import InsStack from '../molecules/InsStack'
+import InsCategories from '../organisms/InsCategories'
+import InsCommunity from '../organisms/InsCommunity'
+import InsHero from '../organisms/InsHero'
 import InsProductCards from '../organisms/InsProductCards'
-import InsProductDetails from '../organisms/InsProductDetails'
-import InsProductHero from '../organisms/InsProductHero'
 
 interface IProps {
   loading: boolean
-  product: IProduct | null
-  similarProducts: IProductItem[]
+  categories: ICategory[]
+  communityImages: ICommunity[]
+  featuredProducts: IProductItem[]
   hikingProducts: IProductItem[]
-  gear: IProductItem[]
+  hero: IHeroWithButton | null
 }
 
-const InsProduct = ({
-  loading = true,
-  product = null,
-  similarProducts = [],
-  gear = [],
+const InsHome = ({
+  loading,
+  categories = [],
+  communityImages = [],
+  featuredProducts = [],
   hikingProducts = [],
+  hero = null,
 }: PropsWithChildren<IProps>): JSX.Element => {
   return (
     <InsIndicator isLoading={loading}>
       <InsStack spacing={10}>
-        <InsProductHero product={product} />
-        <InsProductDetails product={product} />
-        <InsProductCards title="You May Also Like" items={similarProducts} />
-        <InsProductCards title="Complete Your Ride" items={gear} />
+        <InsHero hero={hero} />
+        <InsProductCards title="Featured Bikes" items={featuredProducts} />
+        <InsCategories items={categories} />
         <InsProductCards
           title="Extend Your Journey"
           items={hikingProducts}
           height={275}
         />
+        <InsCommunity images={communityImages} />
       </InsStack>
     </InsIndicator>
   )
 }
 
-export default InsProduct
+export default InsHome
